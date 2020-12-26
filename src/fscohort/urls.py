57826519b481fcf5caf1ba2537669eac4616f494
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-26 20:38:15 +0300

    added studeny_list

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view, about
+from .views import home_view, student_list
 
 urlpatterns = [
     path("", home_view),
-    path("about/", about)
+    path("list/", student_list),
+    # path("about/", about)
 ]
