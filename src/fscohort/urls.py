679876402ed4c99d1e3b0c5204f24f0425df6d21
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-26 21:32:26 +0300

    added student_detail

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_view, student_list, student_add
+from .views import home_view, student_list, student_add, student_detail
 
 urlpatterns = [
-    path("", home_view),
-    path("list/", student_list),
-    path("add/", student_add),
+    path("", home_view, name="home"),
+    path("list/", student_list, name="list"),
+    path("add/", student_add, name="add"),
+    path("<int:id>", student_detail, name="detail"),
     # path("about/", about)
 ]
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import StudentForm
 from .models import Student
@@ -38,8 +38,17 @@ def student_add(request):
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect("home")
 
     context = {
         'form': form
     }
     return render(request, "fscohort/student_add.html", context)
+
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+    context = {
+        'student': student
+    }
+    return render(request, "fscohort/student_detail.html", context)
