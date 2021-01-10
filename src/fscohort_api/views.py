b6be066d1afddb1e11d2a0a4e2dc3c3c90eaa43e
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-10 20:41:30 +0300

    added create view

--- src/fscohort_api/views.py
+++ src/fscohort_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from fscohort.models import Student
 from django.core.serializers import serialize
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home_api(request):
@@ -44,3 +46,29 @@ def student_list_api(request):
             "count": student_count
         }
         return JsonResponse(data)
+
+
+@csrf_exempt
+def student_create_api(request):
+    if request.method == "POST":
+        post_body = json.loads(request.body)
+        print(post_body)
+        print(type(post_body))
+
+        name = post_body.get("first_name")
+        lastname = post_body.get("last_name")
+        number = post_body.get("number")
+
+        student_data = {
+            "first_name": name,
+            "last_name": lastname,
+            "number": number
+        }
+        # print(**student_data)
+
+        student_obj = Student.objects.create(
+            **student_data)  # "firts_name": name, "last_name": lastname,
+        data = {
+            "message": f"Student {student_obj.first_name} created succesfully "
+        }
+        return JsonResponse(data, status=201)
