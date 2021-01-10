2e80e36e2dca06f327e7781c5721c9e73e29ef5b
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-10 17:16:22 +0300

    added home_api

--- src/clarusway/urls.py
+++ src/clarusway/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("fscohort.urls"))
+    path("", include("fscohort.urls")),
+    path("api/", include("fscohort_api.urls"))
 ]
