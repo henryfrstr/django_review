501baab57547d4e2ad1501dae460c372a516cc29
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-10 17:46:18 +0300

    added student list

--- src/fscohort_api/views.py
+++ src/fscohort_api/views.py
@@ -1,11 +1,46 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from fscohort.models import Student
+from django.core.serializers import serialize
 
 
 def home_api(request):
     data = {
         "name": "henry",
-        "adress": "clarusway.com",
+        "address": "clarusway.com",
         "skills": ["python", "django"]
     }
     return JsonResponse(data)
+
+
+# def student_list_api(request):
+#     if request.method == "GET":
+#         students = Student.objects.all()
+#         student_count = Student.objects.count()
+
+#         student_list = []
+#         for student in students: # mevlüt, henry
+#             student_list.append({
+#                 "fisrtname": student.first_name,
+#                 "lastname": student.last_name,
+#                 "number": student.number
+#             })
+#         print(student_list)
+
+#         data = {
+#             "students": student_list,
+#             "count": student_count
+#         }
+#         return JsonResponse(data)
+
+def student_list_api(request):
+    if request.method == "GET":
+        students = Student.objects.all()
+        student_count = Student.objects.count()
+        student_data = serialize("python", students)
+        print(student_data)
+        data = {
+            "students": student_data,
+            "count": student_count
+        }
+        return JsonResponse(data)
